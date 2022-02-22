be2260e67382fc029646913a4e2665a2395e909f
Amey5968 <ameysatpute100@gmail.com>
2022-02-22 23:44:20 +0530

    JS : hoisting , Scope

---
+++ Class_Lec/Javascript/hoisting.js
@@ -0,0 +1,24 @@
+
+console.log(a); // ans : undefined;
+greet();
+
+// hello();
+var a = 10;
+
+// function;
+function greet(){
+    console.log("hello Coders");
+}
+
+// function expression
+var hello = function () { // variable ke andar function hai;
+    console.log("you guys rocked it");
+}
+hello();
+
+
+
+
+// main advantage of hoisting is that code dosent stop means reduces error,
+// as print a which is not declared and initialized 
+// still code is not giving error;
